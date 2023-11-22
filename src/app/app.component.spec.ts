e209881fe90918a3b983283378d2925b09e85c4b
Saintue <bambo20023@gmail.com>
2023-11-22 18:16:43 +0300

    shop/1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'placeholder'`, () => {
+  it(`should have as title 'shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('placeholder');
+    expect(app.title).toEqual('shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('placeholder app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shop app is running!');
   });
 });
